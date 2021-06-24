1559aca0908704a657b1054642ba4ef63c94d294
pommevilla <pvillanueva13@gmail.com>
2021-06-24 15:11:52 -0500

    Add non-verbose to wget

--- quarto-install.sh
+++ quarto-install.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 set -e
 
-echo "Entered"
-
 # Pinning to latest release at time of writing
 quarto_file_name="quarto-0.1.292-amd64.deb"
 quarto_url="https://github.com/quarto-dev/quarto-cli/releases/download/v0.1.292/${quarto_file_name}"
 
-wget "${quarto_url}"
+wget --no-verbose "${quarto_url}"
 
 dpkg -i "${quarto_file_name}"
 

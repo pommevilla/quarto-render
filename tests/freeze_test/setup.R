4218d0dd90006a4d8c692a97fc5150f4c21f9ba0
pommevilla <pvillanueva13@gmail.com>
2021-06-25 15:56:11 -0500

    Render freeze

--- tests/freeze_test/setup.R
+++ tests/freeze_test/setup.R
@@ -17,13 +17,20 @@ library(ggtext)
 
 theme_set(theme_minimal())
 
-### Data
+### Read in Data
 
-data.priming <- read.csv(here("data", "priming_amoA_deltaCt.csv"), header = T) %>% 
+# data.priming <- read.csv(here("data", "priming_amoA_deltaCt.csv"), header = T) %>% 
+#   rename(sample_id = X) 
+# 
+# data.raw <- read.csv(here("data", "priming_amoA_rawCt.csv"), header = T) %>% 
+#   rename(sample_id = X)
+
+data.priming <- read.csv("data/priming_amoA_deltaCt.csv", header = T) %>% 
   rename(sample_id = X) 
 
-data.raw <- read.csv(here("data", "priming_amoA_rawCt.csv"), header = T) %>% 
+data.raw <- read.csv("data/priming_amoA_rawCt.csv", header = T) %>% 
   rename(sample_id = X)
+### Analysis
 
 data.priming.long <- data.priming %>% 
   pivot_longer(cols = amoA.001:amoA.078, names_to = "amoA", values_to = "deltaCT")
